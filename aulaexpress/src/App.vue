aede23fd5c584ab17a3ddc3b836c2930f277a009
Duglas <duglasphone@gmail.com>
2023-06-04 21:07:40 -0500

    Subiendo carpeta a GitHub

---
+++ aulaexpress/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  <nav>
+  <ul>
+  <li><router-link to="/">Inicio</router-link></li>
+  <li><router-link to="/opcion1">Opcion 1</router-link></li>
+  <li><router-link to="/opcion2">Opcion 2</router-link></li>
+  <li><router-link to="/opcion3">Opcion 3</router-link></li>
+  </ul>
+  </nav>
+  <router-view></router-view>
+  </div>
+  </template>
+  <script>
+  export default {
+  name: 'App',
+  };
+  </script>
+  <style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-top: 60px;
+  }
+  ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+margin-bottom: 10px;
+}
+a {
+text-decoration: none;
+}
+</style>
---
+++ aulaexpress/src/components/Opcion3.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    <h1>Este es el componente {{ name }}</h1>
+    </div>
+    </template>
+    <script>
+    export default {
+    name: '{{ name }}'
+    };
+    </script>
+    <style>
+    </style>
